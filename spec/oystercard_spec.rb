4e9ae28fa013e3a2d19398230f2b5c2b2186a924
Jack Oddy <jackoddy91@gmail.com>
2016-07-11 16:05:17 +0100

    sets up balance on Oystercard class

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,4 +2,12 @@ require 'oystercard'
 
 describe Oystercard do
 
+  it { is_expected.to respond_to(:balance) }
+
+  describe '#balance' do
+    it 'returns balance of oystercard' do
+      expect(subject.balance).to eq 0
+    end
+  end
+
 end
