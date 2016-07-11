24e59265d8edeb91c580a48ae807e511e8999e10
Jack Oddy <jackoddy91@gmail.com>
2016-07-11 17:00:33 +0100

    adds top_off functionality to Oystercard

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,12 +1,25 @@
 require 'oystercard'
 
 describe Oystercard do
+  let(:subject) { Oystercard.new }
 
-  it { is_expected.to respond_to(:balance) }
 
   describe '#balance' do
+
+    it { is_expected.to respond_to(:balance) }
+
     it 'returns balance of oystercard' do
       expect(subject.balance).to eq 0
+    end
+  end
+
+  describe'#top_up' do
+    
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+
+    it 'tops up card balance' do
+      subject.top_up(10.00)
+      expect(subject.balance).to eq 10.00
     end
   end
 
