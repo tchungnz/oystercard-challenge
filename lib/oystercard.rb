0da01cb919add16110e5546396c8babbe31d81c9
Michael <mjosephmiller.uk@gmail.com>
2016-07-11 15:51:58 +0100

    adds an initialize method and sets a card balance of zero, also adds an attr_reader to access the value' git push origin day-1

---
+++ lib/oystercard.rb
@@ -0,0 +1,9 @@
+class Oystercard
+
+  attr_reader :get_balance
+
+  def initialize
+    @get_balance = 0
+  end
+
+end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,10 @@
+require 'oystercard'
+
 describe Oystercard do
-  
+
+  it 'get_balance' do
+    oystercard = Oystercard.new
+    expect(oystercard.get_balance).to eq 0
+  end
+
 end
