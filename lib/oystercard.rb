75fc7257d897e36e7134174960a5f219fff2fae1
William Osborne <wosborne@live.co.uk>
2016-07-12 12:24:29 +0100

    create and test top_up, deduct, touch_in, touch_out and in_journey? methods

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,13 +1,33 @@
 class Oystercard
 
+  LIMIT = 90
+
   attr_reader :balance
 
   def initialize
     @balance = 0
+    @in_journey = false
   end
 
   def top_up(amount)
+    fail "Exceeded balance limit of £#{LIMIT}" if (@balance + amount) > LIMIT
     @balance += amount
+  end
+
+  def deduct(fare)
+    @balance -= fare
+  end
+
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
+  end
+
+  def in_journey?
+    @in_journey
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -14,13 +14,49 @@ describe Oystercard do
   end
 
   describe'#top_up' do
-    
+
     it { is_expected.to respond_to(:top_up).with(1).argument }
 
     it 'tops up card balance' do
       subject.top_up(10.00)
       expect(subject.balance).to eq 10.00
     end
+
+    it 'raises error if balance limit exceeded' do
+      subject.top_up(80)
+      expect{subject.top_up(20)}.to raise_error "Exceeded balance limit of £#{Oystercard::LIMIT}"
+    end
   end
 
+  describe '#deduct' do
+    it {is_expected.to respond_to(:deduct).with(1).argument }
+
+    it "deducts fare from balance" do
+      subject.top_up(80)
+      expect(subject.deduct(20)).to eq 60
+    end
+  end
+
+   describe '#touch_in' do
+     it "sets in_journey to true" do
+       subject.touch_in
+       expect(subject).to be_in_journey
+     end
+   end
+
+   describe '#touch_out' do
+     it "sets in_journey to false" do
+       subject.touch_out
+       expect(subject).not_to be_in_journey
+     end
+   end
+
+   describe '#in_journey?' do
+     it "returns in journey status" do
+       subject.touch_in
+       expect(subject.in_journey?).to eq true
+       subject.touch_out
+       expect(subject.in_journey?).to eq false
+     end
+   end
 end
