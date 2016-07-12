fdb1f400b6c0b9c2bb8d95ea99ebd3825c1dc792
Jack Oddy <jackoddy91@gmail.com>
2016-07-12 15:30:17 +0100

    implements a minimum balance restriction + fail when attempting to touch in when balance is below it

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,10 +1,11 @@
 class Oystercard
-CREDIT_LIMIT = 90
+MAX_BALANCE = 90
+MIN_BALANCE = 1
   attr_reader :balance
 
   def initialize
     @balance = 0
-    @in_use = false
+    @in_journey = false
   end
 
   def top_up(amount)
@@ -12,28 +13,39 @@ CREDIT_LIMIT = 90
     @balance += amount
   end
 
-  def balance_exceeded?(amount)
-    @balance + amount > CREDIT_LIMIT
-  end
-
-  def top_up_error
-    "Balance cannot exceed #{CREDIT_LIMIT}."
-  end
-
   def deduct(amount)
     @balance -= amount
   end
 
   def touch_in
-    @in_use = true
+    fail min_balance_error if below_min?
+    @in_journey = true
   end
 
   def touch_out
-    @in_use = false
+    @in_journey = false
   end
 
-  def in_use?
-    @in_use
+  def in_journey?
+    @in_journey
+  end
+
+  private
+
+  def below_min?
+    balance < MIN_BALANCE
+  end
+
+  def min_balance_error
+    "Balance is below £#{MIN_BALANCE}"
+  end
+
+  def balance_exceeded?(amount)
+    balance + amount > MAX_BALANCE
+  end
+
+  def top_up_error
+    "Balance cannot exceed #{MAX_BALANCE}."
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,67 +5,60 @@ describe Oystercard do
 
 
   describe '#balance' do
-
     it { is_expected.to respond_to(:balance) }
-
     it 'returns balance of oystercard' do
       expect(subject.balance).to eq 0
     end
   end
 
   describe'#top_up' do
-
     it { is_expected.to respond_to(:top_up).with(1).argument }
-
     it 'tops up card balance' do
-      expect{ subject.top_up(10.00) }.to  change{ subject.balance }.by 10.00
+      expect{ subject.top_up(10) }.to  change{ subject.balance }.by 10
     end
-
     it 'cannot exceed the credit limit' do
-      max_out = described_class::CREDIT_LIMIT
+      max_out = described_class::MAX_BALANCE
       subject.top_up max_out
       expect{subject.top_up(0.01)}.to raise_error "Balance cannot exceed #{max_out}."
     end
-
   end
 
   describe'#deduct' do
-
     before do
-      subject.top_up 20.00
+      subject.top_up 20
     end
-
     it 'deducts money from balance' do
-      expect{ subject.deduct(10.00) }.to change{ subject.balance }.by -10.00
+      expect{ subject.deduct(10) }.to change{ subject.balance }.by -10
     end
   end
 
   describe '#touch_in' do
-
     it "records a journey as in progress" do
+      subject.top_up(1)
       expect(subject.touch_in).to(eq(true))
     end
-
+    it 'raises error if balance is below a minimum amount' do
+      expect{subject.touch_in}.to(raise_error("Balance is below £#{described_class::MIN_BALANCE}"))
+    end
   end
 
   describe '#touch_out' do
-
     it "ends an in progress journey" do
       expect(subject.touch_out).to(eq(false))
     end
-
   end
 
-  describe '#in_use?' do
+  describe '#in_journey' do
     before do
+      subject.top_up(10)
       subject.touch_in
     end
     it "is true when a card has been touched in" do
-      expect(subject).to(be_in_use)
+      expect(subject).to(be_in_journey)
     end
     it "is false when a card has been touched out" do
       subject.touch_out
-      expect(subject).not_to(be_in_use)
+      expect(subject).not_to(be_in_journey)
     end
   end
 
