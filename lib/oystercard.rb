2d4c097ac6b0b15f9df4adadc325daeffd0a6f1e
Michael <mjosephmiller.uk@gmail.com>
2016-07-12 11:55:21 +0100

    enforces maximum balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,12 +2,15 @@ class Oystercard
 
   attr_reader :get_balance
 
+  MAXIMUM_CAPACITY = 90
+
   def initialize
     @get_balance = 0
   end
 
   def top_up(amount)
     @get_balance += amount
+    raise "maximum balance is #{MAXIMUM_CAPACITY}" if @get_balance > MAXIMUM_CAPACITY
   end
 
 end
