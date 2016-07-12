034031c5c9062e6e576edc6ab6c6ffbaba51f674
Jack Oddy <jackoddy91@gmail.com>
2016-07-12 11:56:16 +0100

    implements a maximum limit on balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,5 +1,5 @@
 class Oystercard
-
+CREDIT_LIMIT = 90
   attr_reader :balance
 
   def initialize
@@ -7,7 +7,16 @@ class Oystercard
   end
 
   def top_up(amount)
+    fail top_up_error if balance_exceeded?(amount)
     @balance += amount
+  end
+
+  def balance_exceeded?(amount)
+    @balance + amount > CREDIT_LIMIT
+  end
+
+  def top_up_error
+    "Balance cannot exceed #{CREDIT_LIMIT}."
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -14,12 +14,18 @@ describe Oystercard do
   end
 
   describe'#top_up' do
-    
+
     it { is_expected.to respond_to(:top_up).with(1).argument }
 
     it 'tops up card balance' do
       subject.top_up(10.00)
       expect(subject.balance).to eq 10.00
+    end
+
+    it 'cannot exceed the credit limit' do
+      max_out = described_class::CREDIT_LIMIT
+      subject.top_up max_out
+      expect{subject.top_up(0.01)}.to raise_error "Balance cannot exceed #{max_out}."
     end
   end
 
